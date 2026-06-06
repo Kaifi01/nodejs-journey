ac5dec2a4afc0132a029e03f3192f6b22458b311
Kaifi@01 <kaifimallick2001@gmail.com>
2026-06-06 21:59:12 +0530

    first commit

---
+++ pactice-1/index.js
@@ -0,0 +1,6 @@
+// const s1=require("./student");
+const {degree}=require('./student')
+// console.log(s1.getName());
+// console.log(s1.getage());
+// console.log(s1.cgpa);
+console.log(degree());
---
+++ pactice-1/student.js
@@ -0,0 +1,21 @@
+const getname=()=>{
+    return "Kaifi"
+}
+
+const getage=()=>{
+    return 25
+}
+const cgpa=8.5;
+exports.degree=()=>{
+    return "BCA"
+}
+
+// exports.getName=getage;
+// exports.getName=getname;
+// exports.cgpa=cgpa;
+// module.exports={
+//     getname,
+//     getage,
+//     cgpa,
+   
+// }
